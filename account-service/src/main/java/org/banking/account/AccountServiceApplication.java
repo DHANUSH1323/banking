0ef5d3f7b5ec030ca6d38006d96aa1f72bb28862
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2025-11-06 18:10:52 -0500

    Configured Yaml file for account service

--- account-service/src/main/java/org/banking/account/AccountServiceApplication.java
+++ account-service/src/main/java/org/banking/account/AccountServiceApplication.java
@@ -1,4 +1,4 @@
-package com.banking.account;
+package org.banking.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
