1d27b51f09f91c1eee2eadabcfe3db9079dd5061
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2025-11-10 14:01:31 -0500

    Completed Transaction Mapper

--- transaction-service/src/main/java/org/banking/transaction/model/mapper/TransactionMapper.java
+++ transaction-service/src/main/java/org/banking/transaction/model/mapper/TransactionMapper.java
@@ -1,5 +1,30 @@
 package org.banking.transaction.model.mapper;
 
-public class TransactionMapper {
+import java.util.Objects;
 
+import org.banking.transaction.model.dto.TransactionDto;
+import org.banking.transaction.model.entity.Transaction;
+import org.springframework.beans.BeanUtils;
+
+public class TransactionMapper extends BaseMapper<Transaction, TransactionDto> {
+
+    @Override
+    public Transaction convertToEntity(TransactionDto dto, Object... args) {
+
+        Transaction transaction = new Transaction();
+        if (!Objects.isNull(dto)) {
+            BeanUtils.copyProperties(dto, transaction);
+        }
+        return transaction;
+    }
+
+    @Override
+    public TransactionDto convertToDto(Transaction entity, Object... args) {
+
+        TransactionDto transactionDto = new TransactionDto();
+        if (!Objects.isNull(entity)) {
+            BeanUtils.copyProperties(entity, transactionDto);
+        }
+        return transactionDto;
+    }
 }
