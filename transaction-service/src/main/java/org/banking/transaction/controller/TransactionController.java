324a968f3f2fa6efbd27c5f961670be9c027343c
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2025-11-11 15:12:24 -0500

    Completed the controller layer

---
+++ transaction-service/src/main/java/org/banking/transaction/controller/TransactionController.java
@@ -0,0 +1,42 @@
+package org.banking.transaction.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.banking.transaction.model.dto.TransactionDto;
+import org.banking.transaction.model.response.Response;
+import org.banking.transaction.model.response.TransactionRequest;
+import org.banking.transaction.service.TransactionService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Response> addTransactions(@RequestBody TransactionDto transactionDto) {
+        return new ResponseEntity<>(transactionService.addTransaction(transactionDto), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/internal")
+    public ResponseEntity<Response> makeInternalTransaction(@RequestBody List<TransactionDto> transactionDtos, @RequestParam String transactionReference) {
+        return new ResponseEntity<>(transactionService.internalTransaction(transactionDtos, transactionReference), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionRequest>> getTransactions(@RequestParam String accountId) {
+        return new ResponseEntity<>(transactionService.getTransaction(accountId), HttpStatus.OK);
+    }
+
+    @GetMapping("/{referenceId}")
+    public ResponseEntity<List<TransactionRequest>> getTransactionByTransactionReference(@PathVariable String referenceId) {
+        return new ResponseEntity<>(transactionService.getTransactionByTransactionReference(referenceId), HttpStatus.OK);
+    }
+}
