3b292e5493200226ceac8854bf0f0f374bed52cd
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2025-11-11 16:03:54 -0500

    changed file path for fund transfer and sequence generator

--- fund-transfer-service/src/main/java/org/banking/transfer/FundTransferServiceApplication.java
+++ fund-transfer-service/src/main/java/org/banking/transfer/FundTransferServiceApplication.java
@@ -1,4 +1,4 @@
-package com.banking.transfer;
+package org.banking.transfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundTransferServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FundTransferServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FundTransferServiceApplication.class, args);
+    }
 
 }
--- sequence-generator/src/main/java/org/banking/sequence/SequenceGeneratorApplication.java
+++ sequence-generator/src/main/java/org/banking/sequence/SequenceGeneratorApplication.java
@@ -1,4 +1,4 @@
-package com.banking.sequence;
+package org.banking.sequence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SequenceGeneratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SequenceGeneratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SequenceGeneratorApplication.class, args);
+    }
 
 }
