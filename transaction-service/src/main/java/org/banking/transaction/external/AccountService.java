873f58d2738cfab3d12c3fdd87a61ef26182b7e6
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2025-11-11 15:04:30 -0500

    External service is completed

---
+++ transaction-service/src/main/java/org/banking/transaction/external/AccountService.java
@@ -0,0 +1,18 @@
+package org.banking.transaction.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.training.transactions.configuration.FeignClientConfiguration;
+import org.banking.transaction.model.external.Account;
+import org.banking.transaction.model.response.Response;
+
+@FeignClient(name = "account-service", configuration = FeignClientConfiguration.class)
+public interface AccountService {
+
+    @GetMapping("/accounts")
+    ResponseEntity<Account> readByAccountNumber(@RequestParam String accountNumber);
+
+    @PutMapping("/accounts")
+    ResponseEntity<Response> updateAccount(@RequestParam String accountNumber, @RequestBody Account account);
+}
